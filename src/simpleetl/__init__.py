9909b704d72d5068d139480e512cf8aa5159295d
Arda Yildirim <yildirim.arda@gmail.com>
2026-09-02 00:24:59 +0200

    chore(main): release 0.3.0

--- src/simpleetl/__init__.py
+++ src/simpleetl/__init__.py
@@ -30,7 +30,7 @@ from importlib.metadata import version as _pkg_version
 try:
     __version__ = _pkg_version("simpleetl")
 except PackageNotFoundError:  # running from a source tree without install
-    __version__ = "0.2.0"
+    __version__ = "0.3.0"
 __author__ = "SimpleETL Contributors"
 
 # -- Core ETL ----------------------------------------------------------------
